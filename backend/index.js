c809f172bfff70adaccd5c7028ca43c87fff3908
Al Amin ET <alaminet.bd@gmail.com>
2024-04-29 23:20:24 +0600

    mongodb-connected

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const route = require("./route");
+const cors = require("cors");
+const mongoConfig = require("./config/mongoConfig");
 const app = express();
+
+// dbconnection
+mongoConfig();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// api router
+app.use(process.env.API_URL, route);
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+// port connection
 port = process.env.PORT || 8000;
 app.listen(port, (req, res) => {
   console.log(`Server Run at ${port}`);
